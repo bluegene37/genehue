64a4e039ff63eef146c064b4072eefb49d0b4363
bluegene37 <bluegene37@outlook.com>
2016-11-28 21:44:58 +0800

    my latest commit

---
+++ js/signature.js
@@ -0,0 +1,30 @@
+(function() {
+    var app;
+
+    $(document).ready(function() {
+        return app.init();
+    });
+
+    app = {
+        text: " Hello stranger...Welcome! How are you doing? ",
+        index: 0,
+        chars: 0,
+        speed: 80,
+        container: ".text .content",
+        init: function() {
+            this.chars = this.text.length;
+            return this.write();
+        },
+
+        write: function() {
+            $(this.container).append(this.text[this.index]);
+            if (this.index < this.chars) {
+                this.index++;
+                return window.setTimeout(function() {
+                    return app.write();
+                }, this.speed);
+            }
+        }
+    };
+
+}).call(this);
